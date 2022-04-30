8306f665e8fd676a084a4d4c0aefce26a43acd8f
phdevbr <pedroheringer2020@gmail.com>
2022-04-30 14:25:08 -0300

    Formulário de Projetos funcionando

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,20 +1,34 @@
 <template>
-    <section class="projetos">
+  <section class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="">
-        <div class="field">
-            <label for="nomeDoProjeto" class="label" >
-                Nome do Projeto
-            </label>
-            <input type="text" class="input" v-model="nomeDoProjeto" />
-        </div>
-        <div class="field">
-            <button class="button" type="submit">
-                Salvar
-            </button>
-        </div>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input type="text" class="input" v-model="nomeDoProjeto" />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
     </form>
-    </section>
+    <table class="table is-fullwidth">
+        <thead>
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Nome do Projeto
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="projeto in projetos" :key="projeto.id">
+                <td>{{projeto.id}}</td>
+                <td>{{projeto.nome}}</td>
+            </tr>
+        </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
@@ -22,22 +36,28 @@ import { defineComponent } from "@vue/runtime-core";
 import IProjeto from "../interfaces/IProjeto";
 
 export default defineComponent({
-    name: "ProjetosView",
-    data() {
-        return {
-            nomeDoProjeto: "",
-            projetos: [] as IProjeto[]
-        };
+  name: "ProjetosView",
+  data() {
+    return {
+      nomeDoProjeto: "",
+      projetos: [] as IProjeto[],
+    };
+  },
+  methods: {
+    salvar() {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString(),
+      };
+      this.projetos.push(projeto);
+      this.nomeDoProjeto = "";
     },
-    methods: {
-        salvar() {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto);
-            this.nomeDoProjeto = "";
-        }
-    },
+  },
 });
 </script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
